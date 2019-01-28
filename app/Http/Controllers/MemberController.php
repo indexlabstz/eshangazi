c921157e9133dd77a919f7bf7a80125a42be5b8b
Juma Hakika <mohamedhakika@gmail.com>
2019-01-28 15:10:24 +0300

    Debuging saving user info

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -93,7 +93,7 @@ class MemberController extends Controller
             if ($incomplete) {
                 $bot->reply($apiReply);
             } else {
-                \Log::debug('Nimeingia hapa kwenye kusave');
+                \Log::debug('Nimeingia hapa kwenye kusave '.$user->getInfo());
                 $this->subscribeWithNoData($user, $extras, $driver);
 
                 if ($driver === 'Facebook') {
