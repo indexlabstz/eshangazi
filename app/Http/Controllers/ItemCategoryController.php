356cb55527f11756cdaeaa2b3ecd8f649dc179d8
Juma Hakika <mohamedhakika@gmail.com>
2019-02-09 12:03:01 +0300

    Chunk item desciption

--- app/Http/Controllers/ItemCategoryController.php
+++ app/Http/Controllers/ItemCategoryController.php
@@ -270,7 +270,7 @@ class ItemCategoryController extends Controller
     public function toFacebook($category)
     {
         \Log::debug('itemdescategory: '.$category->description);
-        $descriptions= explode("\n", $category->description);
+        $descriptions= explode("\r\n", $category->description);
         \Log::debug('categoryarray', $descriptions);
         $items = Item::where('item_category_id', $category->id)->where('item_id', NULL)->inRandomOrder()->take(3)->get();
 
