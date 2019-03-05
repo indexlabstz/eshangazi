6755a1ca0bf8bb23eb003380967c707b216a807d
Yesaya R. Athuman <yesayaathuman@gmail.com>
2019-03-05 14:01:38 +0300

    Mix version

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,3 +16,7 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .tailwind()
     .browserSync('eshangazi.test');
+
+if (mix.inProduction()) {
+    mix.version();
+}
