ef89df01fb6eb78eeb162f9a74b73cc40d3a9e2e
Yesaya R. Athuman <yesayaathuman@gmail.com>
2019-08-22 03:05:35 +0300

    Set upload to public

--- config/filesystems.php
+++ config/filesystems.php
@@ -71,7 +71,8 @@ return [
             'key' => env('S3_KEY'),
             'secret' => env('S3_SECRET'),
             'region' => env('S3_REGION'),
-            'bucket' => env('S3_BUCKET')
+            'bucket' => env('S3_BUCKET'),
+            'visibility' => 'public',
         ],
 
     ],
